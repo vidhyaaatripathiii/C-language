7cd261e1f1f9c708f73ad08d73acf3b120e92d6c
Vidhya <vidhyaaatripathiii@gmail.com>
2026-07-14 11:59:39 +0530

    Initial commit

---
+++ even_n_odd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0)
+    {
+        printf("%d is an Even number.", num);
+    }
+    else
+    {
+        printf("%d is an Odd number.", num);
+    }
+
+    return 0; 
+}  
---
+++ simple_calculator.c
@@ -0,0 +1,37 @@
+// My first C project
+// Created while learning Git and GitHub
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &op);   // Notice the space before %c
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    switch(op) {
+        case '+':
+            printf("Result = %d", a + b);
+            break;
+        case '-':
+            printf("Result = %d", a - b);
+            break;
+        case '*':
+            printf("Result = %d", a * b);
+            break;
+        case '/':
+            printf("Result = %d", a / b);
+            break;
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
